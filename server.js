93bcdf6c5397ded098afb09ed4003d7301d440cf
AKHIL P SAJI <akhil.sali@Incture.local>
2024-02-19 19:04:15 +0530

    token api added

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-mongoose.connect("mongodb+srv://akhilpsaji:akhilpsaji@mycluster.reur6su.mongodb.net/JOBSEEKING?retryWrites=true&w=majority");
-// mongoose.connect(process.env.MongoDB_URL);
+// mongoose.connect("mongodb+srv://akhilpsaji:akhilpsaji@mycluster.reur6su.mongodb.net/JOBSEEKING?retryWrites=true&w=majority");
+mongoose.connect(process.env.MongoDB_URL);
 app.use("/jobs",jobRoute);
+app.use("/token",async(req,res)=>{
+    res.status(200).json({token:req.headers.authorization});
+})
 app.listen(PORT,()=>{
     console.log(`Server started at PORT:${PORT}`);
 })
